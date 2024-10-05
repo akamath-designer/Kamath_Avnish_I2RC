45f2e9703409a96a46996ed539ec334bf7a1c5de
akamath-designer <avnishkamath@gmail.com>
2024-10-05 13:48:36 -0700

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Preseason Digital Notebook Example
+Name: **[INSERT NAME HERE]**
+
+Section: **[I2RC/ARC]**
+
+Week: **[INSERT WEEK NUM HERE]**
+
+
+## Code
+
+The main topic this week was: **[TOPIC NAME]**
+
+Commands: **[COMMANDS]**
+
+Subsystems: **[SUBSYSTEMS]**
+
+### How does the code work?
+Write about how it works here
+
+
+### Important notes for future reference
+Notes about git, and helpful resources, etc. 
+
+Please put them here, they will really help you in the future 
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.math.util.Units;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+
+public class Drivetrain extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public Drivetrain() {}
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
